83ce0d8d7b4cea08e86d95e4169efd2719310a73
Jorge Kimura <kimurajorge2001@gmail.com>
2022-11-23 12:11:31 -0300

    feat: views get e post finalizadas

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Team
+from django.forms import model_to_dict
+
 
 # Create your views here.
+class TeamView(APIView):
+    def post(self, request):
+        team_data = request.data
+
+        team = Team.objects.create(**team_data)
+
+        return Response(model_to_dict(team), 201)
+
+    def get(self, request):
+
+        team_dict = [team for team in Team.objects.all().values()]
+
+        return Response(team_dict)
